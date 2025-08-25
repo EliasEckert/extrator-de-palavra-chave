ea4a0bb4a8696ea61c60bda5cfe9bb8f177f13f7
EliasEckert <elias.eckert@escola.pr.gov.br>
2025-08-25 08:46:55 -0300

    .

---
+++ script.js
@@ -0,0 +1,13 @@
+const botaoMostra Palavras = document.querySelector("#botao-palavrachave):
+
+botaoMostra Palavras.addEventListener("click", mostra Palavras Chave):
+
+function mostra Palavras Chave) {
+
+const texto = document.querySelector("#entrada-de-texto").value;
+
+const campoResultado = document.querySelector("#resultado-palavrachave"):
+
+const palavrasChave processa Texto(texto);
+
+campo Resultado.textContent = palavras Chave.join(".");                           
